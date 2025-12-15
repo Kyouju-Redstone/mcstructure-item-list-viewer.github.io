3dc5df8ad9c49920a5cf7d0f2e280ccf7e616766
Kyouju-Redstone <kyouju2922@gmail.com>
2025-12-15 10:23:23 +0900

    next.config.tsの環境変数名を修正

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const branchName = process.env.BRANCH_NAME 
-    ? "/" + process.env.BRANCH_NAME 
+const branchName = process.env.REPOSITORY_NAME 
+    ? "/" + process.env.REPOSITORY_NAME 
     : "";
 
-console.log(`BRANCH_NAME: ${process.env.BRANCH_NAME}`);
+console.log(`REPOSITORY_NAME: ${process.env.REPOSITORY_NAME}`);
 
 const nextConfig: NextConfig = {
     output: "export",           // next build 実行の際に Static Export を利用する
