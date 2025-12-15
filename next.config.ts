545db3dee1c0355b264fbbb73c9b9a7d677c4151
Kyouju-Redstone <kyouju2922@gmail.com>
2025-12-15 11:46:18 +0900

    環境変数設定を修正

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
-console.log(`BASE_PATH: ${process.env.NEXT_PUBLIC_BASE_PATH}`);
+console.log(`BASE_PATH: ${basePath}`);
 
 const nextConfig: NextConfig = {
     output: "export",           // next build 実行の際に Static Export を利用する
